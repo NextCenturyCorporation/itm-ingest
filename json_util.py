28991f9a156a16c72fd6de33dc47cf0096c61c36
dereknop <dereknop@gmail.com>
2023-10-24 14:51:02 -0400

    Better documentation

--- json_util.py
+++ json_util.py
@@ -1,16 +1,41 @@
 import json
 
 def read_json_file(file_path):
+    """
+    Read JSON data from a file.
+
+    Args:
+        file_path: Path to the JSON file.
+
+    Returns:
+        Parsed JSON data.
+    """
     with open(file_path, 'r') as file:
         data = json.load(file)
     return data
 
 def is_json_file(file_path):
-    # file extension is ".json"
+    """
+    Check if a file has a JSON file extension.
+
+    Args:
+        file_path: Path to the file.
+
+    Returns:
+        True if the file has a ".json" extension, otherwise False.
+    """
     return file_path.lower().endswith('.json')
 
-# json files will have unique session id. return value of key "session_id"
 def get_json_session_id(json_data):
+    """
+    Extract the session_id from JSON data.
+
+    Args:
+        json_data: JSON data with a "session_id" key.
+
+    Returns:
+        The value of the "session_id" key.
+    """
     try:
         session_id = json_data.get("session_id")
         if session_id is not None:
@@ -19,4 +44,3 @@ def get_json_session_id(json_data):
             raise KeyError("Key 'session_id' not found in the JSON data.")
     except KeyError as e:
         raise e
-
