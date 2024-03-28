f56b5f41c3c89223c66ba025c1a2ae7c07bd99b7
brianpippin <brianpippin@gmail.com>
2024-03-28 13:20:18 -0400

    Add deployment script and update old db records

---
+++ deployment_script.py
@@ -0,0 +1,45 @@
+from pymongo import MongoClient
+from scripts._0_0_2_add_evaluation_data_to_mvps import add_evaluation_name_to_mvp_data
+
+VERSION_COLLECTION = "itm_version"
+
+# Change this version if running a new deploy script
+db_version = "0.0.2"
+
+
+def check_version(mongoDB):
+    collection = mongoDB[VERSION_COLLECTION]
+    version_obj = collection.find_one()
+    if version_obj is None:
+        return True 
+    # return true if it is a newer db version
+    return db_version > version_obj['version']
+
+
+def update_db_version(mongoDB):
+    collection = mongoDB[VERSION_COLLECTION]
+    version_obj = collection.find_one()
+    if version_obj is None:
+        collection.insert_one({"version": db_version})
+    else: 
+        version_obj['version'] = db_version
+        collection.replace_one({"_id": version_obj["_id"]}, version_obj)
+
+
+def main():
+    client = MongoClient(
+        "mongodb://simplemongousername:simplemongopassword@localhost:27017/?authSource=dashboard")
+    mongoDB = client['dashboard']
+    if(check_version(mongoDB)):
+        print("New db version, execute scripts")
+        # Place scripts here to run
+        add_evaluation_name_to_mvp_data(mongoDB)
+
+        # Now update db version
+        update_db_version(mongoDB)
+    else:
+        print("Script does not need to run on prod, already updated.")
+
+
+if __name__ == "__main__":
+    main()
---
+++ scripts/_0_0_2_add_evaluation_data_to_mvps.py
@@ -0,0 +1,31 @@
+def add_evaluation_name_to_mvp_data(mongoDB):
+    scenarios_collection = mongoDB["scenarios"]
+    all_scenarios = scenarios_collection.find({})
+   
+    for scenario in all_scenarios:
+        if scenario["id"] == "ADEPT1" or scenario["id"] == "kickoff-demo-scenario-1":
+            scenario["evalNumber"] = 1
+            scenario["evalName"] = "MVP"
+        else:
+            scenario["evalNumber"] = 2
+            scenario["evalName"] = "September Milestone"
+
+        scenarios_collection.replace_one({"_id": scenario["_id"]}, scenario)
+
+    print("All scenarios updated with eval information")
+
+    test_collection = mongoDB["test"]
+    all_test_records = test_collection.find({})
+
+    for test_record in all_test_records:
+        if isinstance( test_record["history"][0]["response"], dict):
+            if test_record["history"][0]["response"]["id"]  == "ADEPT1" or test_record["history"][0]["response"]["id"]  == "kickoff-demo-scenario-1":
+                test_record["evalNumber"] = 1
+                test_record["evalName"] = "MVP"
+        else:
+            test_record["evalNumber"] = 2
+            test_record["evalName"] = "September Milestone"
+
+        test_collection.replace_one({"_id": test_record["_id"]}, test_record)
+
+    print("All test records updated with eval information")
