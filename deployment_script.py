2704666b4371afaabef259fcc53e0fe28a6b5003
brianpippin <brianpippin@gmail.com>
2024-04-10 09:08:29 -0400

    Script to remove old ADM runs and add new ADM runs

--- deployment_script.py
+++ deployment_script.py
@@ -1,11 +1,13 @@
 from pymongo import MongoClient
 import os
+from scripts._0_0_3_add_eval_3_adm_data import add_eval_3_adm_data
 from scripts._0_0_4_update_human_sim_records import update_hum_sim_order_avg_across_scenes
+from scripts._0_0_5_remove_old_adm_data import remove_old_adm_records
 
 VERSION_COLLECTION = "itm_version"
 
 # Change this version if running a new deploy script
-db_version = "0.0.4"
+db_version = "0.0.5"
 
 
 def check_version(mongoDB):
@@ -33,8 +35,11 @@ def main():
     mongoDB = client['dashboard']
     if(check_version(mongoDB)):
         print("New db version, execute scripts")
+
         # Place scripts here to run
         update_hum_sim_order_avg_across_scenes(mongoDB)
+        remove_old_adm_records(mongoDB)
+        add_eval_3_adm_data(mongoDB)
 
         # Run Script for Probe Matching and importing human data
         os.system("python3 probe_matcher.py -i metrics-data/")
