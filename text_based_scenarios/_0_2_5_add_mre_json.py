9f3cd06c3d4c2483cdb50bfc89e77009ec1864d7
dereknop <dereknop@gmail.com>
2024-09-23 12:00:02 -0400

    ingest necessary files

---
+++ text_based_scenarios/_0_2_5_add_mre_json.py
@@ -0,0 +1,22 @@
+import json
+import os
+
+def add_mre_json(mongo_db):
+    text_based_config = mongo_db['textBasedConfig']
+    
+    current_script_dir = os.path.dirname(os.path.abspath(__file__))
+    json_folder = os.path.join(current_script_dir, 'mre-json-files')
+    
+    # Get all JSON files in the folder
+    json_files = [f for f in os.listdir(json_folder) if f.endswith('.json')]
+    
+    for file in json_files:
+        file_path = os.path.join(json_folder, file)
+        with open(file_path, 'r') as f:
+            data = json.load(f)
+        
+        data['eval'] = 'mre-eval'
+        # Insert the data into the collection
+        result = text_based_config.insert_one(data)
+        print(f"Inserted document: {file}")
+    
