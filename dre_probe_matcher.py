7929166043583b4e77f064ef43045823e08811eb
kaitlyn.sharo <kaitlyn.sharo@caci.com>
2024-09-24 12:43:21 -0400

    fixed minor indexing problem

--- dre_probe_matcher.py
+++ dre_probe_matcher.py
@@ -312,19 +312,19 @@ class ProbeMatcher:
                         action_taken['answer'] = 'U' + action_taken['answer'][1:]
                     answer = ('casualty_' + action_taken['answer'][0]).lower()
                     if answer in actions['Intend']:
-                        last_action_ind_used = ind
+                        last_action_ind_used += ind
                         found_match = True
                         matched = actions['Intend'][answer]
                         break
                 elif action_taken['actionType'] in VITALS_ACTIONS and 'Vitals' in actions:
                     if action_taken['casualty'] in actions['Vitals']:
-                        last_action_ind_used = ind
+                        last_action_ind_used += ind
                         found_match = True
                         matched = actions['Vitals'][action_taken['casualty']]
                         break
                 elif action_taken['actionType'] in actions:
                     if action_taken['casualty'] in actions[action_taken['actionType']]:
-                        last_action_ind_used = ind
+                        last_action_ind_used += ind
                         found_match = True
                         matched = actions[action_taken['actionType']][action_taken['casualty']]
                         break
@@ -360,7 +360,7 @@ class ProbeMatcher:
                             "probe": matched,
                             "user_action": fake_action
                         })
-                        updated_json['actionList'].insert(last_action_ind_used+actions_added+2, fake_action)
+                        updated_json['actionList'].insert(last_action_ind_used+actions_added+1, fake_action)
                         actions_added += 1
 
         print(f"Found {found} out of {total} probes")
