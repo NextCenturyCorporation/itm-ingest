039deebd2406378a7e9768b8974701d784d81e3e
Phi Le <phi.le@caci.com>
2024-11-18 09:22:43 -0500

    added script

---
+++ scripts/_0_3_4_add_eval_5_scenarios.py
@@ -0,0 +1,23 @@
+import os
+import yaml
+
+TA1_FOLDER = "phase1/scenarios/"
+
+def load_ta1_yaml_files(mongoDB):
+    ta1files = [f for f in os.listdir(TA1_FOLDER)]
+    ta1files.sort()
+
+    for file in ta1files:
+        file_name = os.path.join(TA1_FOLDER, file)
+        with open(file_name) as f: 
+            yaml_obj = yaml.safe_load(f)
+            yaml_obj["evalNumber"] = 5
+            yaml_obj["evalName"] = "Phase 1 Evaluation"
+
+            scenarios_collection = mongoDB["scenarios"]
+            scenarios_collection.insert_one(yaml_obj)
+            print("Loaded scenario: " + file_name)
+
+    print("Finished loading TA1 Phase 1 scenario files.")
+
+
