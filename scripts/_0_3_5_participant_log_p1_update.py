f4e1f6a628d435bca8677b2550b042690a7c1442
dereknop <dereknop@gmail.com>
2024-11-19 08:33:46 -0500

    script for updating plog

---
+++ scripts/_0_3_5_participant_log_p1_update.py
@@ -0,0 +1,42 @@
+def participant_log_p1_update(mongo_db):
+    participant_log = mongo_db['participantLog']
+    result = participant_log.delete_many({
+        '$or': [
+            {'claimed': {'$ne': True}},
+            {'claimed': {'$exists': False}}
+        ]
+    })
+
+    example_civ = {
+        'Type': 'Civ',
+        'ParticipantID': 202411101,
+        'Text-1': 'AD-1',
+        'Text-2': 'ST-1',
+        'Sim-1': 'AD-2',
+        'Sim-2': 'ST-2',
+        'Del-1': 'AD-3',
+        'Del-2': 'ST-3',
+        'ADMOrder': 3,
+        'claimed': False,
+        'simEntryCount': 0,
+        'surveyEntryCount': 0,
+        'textEntryCount': 0 
+    }
+
+    example_mil = {
+        'Type': 'Mil',
+        'ParticipantID': 2024011101,
+        'Text-1': 'AD-1',
+        'Text-2': 'ST-1',
+        'Sim-1': 'AD-2',
+        'Sim-2': 'ST-2',
+        'Del-1': 'AD-3',
+        'Del-2': 'ST-3',
+        'ADMOrder': 1,
+        'claimed': False,
+        'simEntryCount': 0,
+        'surveyEntryCount': 0,
+        'textEntryCount': 0 
+    }
+    
+    participant_log.insert_many([example_civ, example_mil])
