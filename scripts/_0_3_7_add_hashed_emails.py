15aaa24eaeab8a067d3f3d952783f830dba0cc47
kaitlyn.sharo <kaitlyn.sharo@caci.com>
2024-11-26 13:17:53 -0500

    add hashed emails to participant log when hashedEmail=null

---
+++ scripts/_0_3_7_add_hashed_emails.py
@@ -0,0 +1,10 @@
+import hashlib
+
+def participant_log_p1_update(mongo_db):
+    participant_log = mongo_db['participantLog']
+    result = participant_log.find({'hashedEmail': None})
+
+    for doc in result:
+        pid = str(doc['ParticipantID']).encode('utf-8')
+        hashed_pid = hashlib.sha256(pid).hexdigest()
+        participant_log.update_one({'_id': doc['_id']}, {'$set': {'hashedEmail': hashed_pid}})
