d026e3d198c185ee0905013956b96af3fa92bbd4
kaitlyn.sharo <kaitlyn.sharo@caci.com>
2025-01-02 18:58:04 -0500

    added st group targets

--- scripts/_0_4_9_ph1_group_targets.py
+++ scripts/_0_4_9_ph1_group_targets.py
@@ -4,64 +4,255 @@ from decouple import config
 ADEPT_URL = config("ADEPT_URL")
 ST_URL = config("ST_URL")
 
-GROUP_TARGETS = {
-    '202411541': ['ADEPT-Phase1Eval-Ingroup Bias-Group-Low'],
-    '202411310': ['ADEPT-Phase1Eval-Ingroup Bias-Group-Low', 'ADEPT-Phase1Eval-Moral judgement-Group-Low'],
-    '202411311': ['ADEPT-Phase1Eval-Ingroup Bias-Group-Low'],
-    '202411536': ['ADEPT-Phase1Eval-Ingroup Bias-Group-Low', 'ADEPT-Phase1Eval-Moral judgement-Group-Low'],
-    '202411315': ['ADEPT-Phase1Eval-Ingroup Bias-Group-Low', 'ADEPT-Phase1Eval-Moral judgement-Group-Low'],
-    '202411559': ['ADEPT-Phase1Eval-Ingroup Bias-Group-Low'],
-    '202411547': ['ADEPT-Phase1Eval-Ingroup Bias-Group-Low', 'ADEPT-Phase1Eval-Moral judgement-Group-High'],
-    '202411577': ['ADEPT-Phase1Eval-Ingroup Bias-Group-Low', 'ADEPT-Phase1Eval-Moral judgement-Group-High'],
-    '202411350': ['ADEPT-Phase1Eval-Ingroup Bias-Group-Low', 'ADEPT-Phase1Eval-Moral judgement-Group-Low'],
-    '202411555': ['ADEPT-Phase1Eval-Ingroup Bias-Group-Low', 'ADEPT-Phase1Eval-Moral judgement-Group-High'],
-    '202411302': ['ADEPT-Phase1Eval-Ingroup Bias-Group-Low', 'ADEPT-Phase1Eval-Moral judgement-Group-High'],
-    '202411567': ['ADEPT-Phase1Eval-Ingroup Bias-Group-Low', 'ADEPT-Phase1Eval-Moral judgement-Group-Low'],
-    '202411312': ['ADEPT-Phase1Eval-Ingroup Bias-Group-Low'],
-    '202411557': ['ADEPT-Phase1Eval-Ingroup Bias-Group-Low'],
-    '202411560': ['ADEPT-Phase1Eval-Ingroup Bias-Group-Low', 'ADEPT-Phase1Eval-Moral judgement-Group-Low'],
-    '202411369': ['ADEPT-Phase1Eval-Ingroup Bias-Group-Low'],
-    '202411338': ['ADEPT-Phase1Eval-Ingroup Bias-Group-Low', 'ADEPT-Phase1Eval-Moral judgement-Group-High'],
-    '202411314': ['ADEPT-Phase1Eval-Ingroup Bias-Group-Low', 'ADEPT-Phase1Eval-Moral judgement-Group-Low'],
-    '202411340': ['ADEPT-Phase1Eval-Ingroup Bias-Group-Low'],
-    '202411514': ['ADEPT-Phase1Eval-Ingroup Bias-Group-Low', 'ADEPT-Phase1Eval-Moral judgement-Group-Low'],
-    '202411544': ['ADEPT-Phase1Eval-Ingroup Bias-Group-Low'],
-    '202411561': ['ADEPT-Phase1Eval-Ingroup Bias-Group-Low', 'ADEPT-Phase1Eval-Moral judgement-Group-Low'],
-
-    '202411545': ['ADEPT-Phase1Eval-Ingroup Bias-Group-High'],
-    '202411334': ['ADEPT-Phase1Eval-Ingroup Bias-Group-High', 'ADEPT-Phase1Eval-Moral judgement-Group-Low'],
-    '202411367': ['ADEPT-Phase1Eval-Ingroup Bias-Group-High', 'ADEPT-Phase1Eval-Moral judgement-Group-High'],
-    '202411351': ['ADEPT-Phase1Eval-Ingroup Bias-Group-High', 'ADEPT-Phase1Eval-Moral judgement-Group-Low'],
-    '202411353': ['ADEPT-Phase1Eval-Ingroup Bias-Group-High', 'ADEPT-Phase1Eval-Moral judgement-Group-Low'],
-    '202411327': ['ADEPT-Phase1Eval-Ingroup Bias-Group-High', 'ADEPT-Phase1Eval-Moral judgement-Group-High'],
-    '202411563': ['ADEPT-Phase1Eval-Ingroup Bias-Group-High'],
-    '202411358': ['ADEPT-Phase1Eval-Ingroup Bias-Group-High', 'ADEPT-Phase1Eval-Moral judgement-Group-Low'],
-    '202411569': ['ADEPT-Phase1Eval-Ingroup Bias-Group-High', 'ADEPT-Phase1Eval-Moral judgement-Group-High'],
-    '202411570': ['ADEPT-Phase1Eval-Ingroup Bias-Group-High'],
-    '202411554': ['ADEPT-Phase1Eval-Ingroup Bias-Group-High', 'ADEPT-Phase1Eval-Moral judgement-Group-High'],
-    '202411572': ['ADEPT-Phase1Eval-Ingroup Bias-Group-High', 'ADEPT-Phase1Eval-Moral judgement-Group-High'],
-    '202411333': ['ADEPT-Phase1Eval-Ingroup Bias-Group-High', 'ADEPT-Phase1Eval-Moral judgement-Group-Low'],
-    '202411579': ['ADEPT-Phase1Eval-Ingroup Bias-Group-High', 'ADEPT-Phase1Eval-Moral judgement-Group-High'],
-    '202411539': ['ADEPT-Phase1Eval-Ingroup Bias-Group-High', 'ADEPT-Phase1Eval-Moral judgement-Group-High'],
-    '202411363': ['ADEPT-Phase1Eval-Ingroup Bias-Group-High'],
-    '202411553': ['ADEPT-Phase1Eval-Ingroup Bias-Group-High', 'ADEPT-Phase1Eval-Moral judgement-Group-Low'],
-    '202411565': ['ADEPT-Phase1Eval-Ingroup Bias-Group-High', 'ADEPT-Phase1Eval-Moral judgement-Group-High'],
-    '202411581': ['ADEPT-Phase1Eval-Ingroup Bias-Group-High', 'ADEPT-Phase1Eval-Moral judgement-Group-High'],
-    '202411359': ['ADEPT-Phase1Eval-Ingroup Bias-Group-High', 'ADEPT-Phase1Eval-Moral judgement-Group-High'],
-    '202411580': ['ADEPT-Phase1Eval-Ingroup Bias-Group-High', 'ADEPT-Phase1Eval-Moral judgement-Group-High'],
-
-    '202411316': ['ADEPT-Phase1Eval-Moral judgement-Group-Low'],
-    '202411346': ['ADEPT-Phase1Eval-Moral judgement-Group-Low'],
-    '202411313': ['ADEPT-Phase1Eval-Moral judgement-Group-Low'],
-    '202411357': ['ADEPT-Phase1Eval-Moral judgement-Group-Low'],
-    '202411543': ['ADEPT-Phase1Eval-Moral judgement-Group-Low'],
-    '202411370': ['ADEPT-Phase1Eval-Moral judgement-Group-Low'],
-
-    '202411578': ['ADEPT-Phase1Eval-Moral judgement-Group-High'],
-    '202411537': ['ADEPT-Phase1Eval-Moral judgement-Group-High'],
-    '202411534': ['ADEPT-Phase1Eval-Moral judgement-Group-High'],
-    '202411550': ['ADEPT-Phase1Eval-Moral judgement-Group-High'],
-    '202411548': ['ADEPT-Phase1Eval-Moral judgement-Group-High']
+GROUP_TARGETS ={
+    "202411541": [
+        "ADEPT-Phase1Eval-Ingroup Bias-Group-Low",
+        "qol-group-target-2-final-eval"
+    ],
+    "202411310": [
+        "ADEPT-Phase1Eval-Ingroup Bias-Group-Low",
+        "ADEPT-Phase1Eval-Moral judgement-Group-Low",
+        "qol-group-target-2-final-eval"
+    ],
+    "202411311": [
+        "ADEPT-Phase1Eval-Ingroup Bias-Group-Low"
+    ],
+    "202411536": [
+        "ADEPT-Phase1Eval-Ingroup Bias-Group-Low",
+        "ADEPT-Phase1Eval-Moral judgement-Group-Low"
+    ],
+    "202411315": [
+        "ADEPT-Phase1Eval-Ingroup Bias-Group-Low",
+        "ADEPT-Phase1Eval-Moral judgement-Group-Low"
+    ],
+    "202411559": [
+        "ADEPT-Phase1Eval-Ingroup Bias-Group-Low",
+        "vol-group-target-2-final-eval"
+    ],
+    "202411547": [
+        "ADEPT-Phase1Eval-Ingroup Bias-Group-Low",
+        "ADEPT-Phase1Eval-Moral judgement-Group-High",
+        "vol-group-target-2-final-eval"
+    ],
+    "202411577": [
+        "ADEPT-Phase1Eval-Ingroup Bias-Group-Low",
+        "ADEPT-Phase1Eval-Moral judgement-Group-High"
+    ],
+    "202411350": [
+        "ADEPT-Phase1Eval-Ingroup Bias-Group-Low",
+        "ADEPT-Phase1Eval-Moral judgement-Group-Low",
+        "qol-group-target-2-final-eval"
+    ],
+    "202411555": [
+        "ADEPT-Phase1Eval-Ingroup Bias-Group-Low",
+        "ADEPT-Phase1Eval-Moral judgement-Group-High",
+        "vol-group-target-2-final-eval"
+    ],
+    "202411302": [
+        "ADEPT-Phase1Eval-Ingroup Bias-Group-Low",
+        "ADEPT-Phase1Eval-Moral judgement-Group-High",
+        "qol-group-target-1-final-eval"
+    ],
+    "202411567": [
+        "ADEPT-Phase1Eval-Ingroup Bias-Group-Low",
+        "ADEPT-Phase1Eval-Moral judgement-Group-Low",
+        "qol-group-target-2-final-eval",
+        "vol-group-target-2-final-eval"
+    ],
+    "202411312": [
+        "ADEPT-Phase1Eval-Ingroup Bias-Group-Low"
+    ],
+    "202411557": [
+        "ADEPT-Phase1Eval-Ingroup Bias-Group-Low",
+        "qol-group-target-2-final-eval"
+    ],
+    "202411560": [
+        "ADEPT-Phase1Eval-Ingroup Bias-Group-Low",
+        "ADEPT-Phase1Eval-Moral judgement-Group-Low"
+    ],
+    "202411369": [
+        "ADEPT-Phase1Eval-Ingroup Bias-Group-Low"
+    ],
+    "202411338": [
+        "ADEPT-Phase1Eval-Ingroup Bias-Group-Low",
+        "ADEPT-Phase1Eval-Moral judgement-Group-High",
+        "qol-group-target-2-final-eval"
+    ],
+    "202411314": [
+        "ADEPT-Phase1Eval-Ingroup Bias-Group-Low",
+        "ADEPT-Phase1Eval-Moral judgement-Group-Low",
+        "vol-group-target-2-final-eval"
+    ],
+    "202411340": [
+        "ADEPT-Phase1Eval-Ingroup Bias-Group-Low",
+        "qol-group-target-1-final-eval"
+    ],
+    "202411514": [
+        "ADEPT-Phase1Eval-Ingroup Bias-Group-Low",
+        "ADEPT-Phase1Eval-Moral judgement-Group-Low"
+    ],
+    "202411544": [
+        "ADEPT-Phase1Eval-Ingroup Bias-Group-Low",
+        "qol-group-target-2-final-eval",
+        "vol-group-target-1-final-eval"
+    ],
+    "202411561": [
+        "ADEPT-Phase1Eval-Ingroup Bias-Group-Low",
+        "ADEPT-Phase1Eval-Moral judgement-Group-Low"
+    ],
+    "202411545": [
+        "ADEPT-Phase1Eval-Ingroup Bias-Group-High",
+        "qol-group-target-2-final-eval",
+        "vol-group-target-1-final-eval"
+    ],
+    "202411334": [
+        "ADEPT-Phase1Eval-Ingroup Bias-Group-High",
+        "ADEPT-Phase1Eval-Moral judgement-Group-Low",
+        "qol-group-target-2-final-eval"
+    ],
+    "202411367": [
+        "ADEPT-Phase1Eval-Ingroup Bias-Group-High",
+        "ADEPT-Phase1Eval-Moral judgement-Group-High",
+        "qol-group-target-2-final-eval",
+        "vol-group-target-1-final-eval"
+    ],
+    "202411351": [
+        "ADEPT-Phase1Eval-Ingroup Bias-Group-High",
+        "ADEPT-Phase1Eval-Moral judgement-Group-Low",
+        "qol-group-target-2-final-eval",
+        "vol-group-target-2-final-eval"
+    ],
+    "202411353": [
+        "ADEPT-Phase1Eval-Ingroup Bias-Group-High",
+        "ADEPT-Phase1Eval-Moral judgement-Group-Low",
+        "vol-group-target-1-final-eval"
+    ],
+    "202411327": [
+        "ADEPT-Phase1Eval-Ingroup Bias-Group-High",
+        "ADEPT-Phase1Eval-Moral judgement-Group-High",
+        "qol-group-target-1-final-eval"
+    ],
+    "202411563": [
+        "ADEPT-Phase1Eval-Ingroup Bias-Group-High",
+        "vol-group-target-1-final-eval"
+    ],
+    "202411358": [
+        "ADEPT-Phase1Eval-Ingroup Bias-Group-High",
+        "ADEPT-Phase1Eval-Moral judgement-Group-Low",
+        "qol-group-target-2-final-eval"
+    ],
+    "202411569": [
+        "ADEPT-Phase1Eval-Ingroup Bias-Group-High",
+        "ADEPT-Phase1Eval-Moral judgement-Group-High"
+    ],
+    "202411570": [
+        "ADEPT-Phase1Eval-Ingroup Bias-Group-High"
+    ],
+    "202411554": [
+        "ADEPT-Phase1Eval-Ingroup Bias-Group-High",
+        "ADEPT-Phase1Eval-Moral judgement-Group-High",
+        "vol-group-target-1-final-eval"
+    ],
+    "202411572": [
+        "ADEPT-Phase1Eval-Ingroup Bias-Group-High",
+        "ADEPT-Phase1Eval-Moral judgement-Group-High",
+        "qol-group-target-1-final-eval"
+    ],
+    "202411333": [
+        "ADEPT-Phase1Eval-Ingroup Bias-Group-High",
+        "ADEPT-Phase1Eval-Moral judgement-Group-Low"
+    ],
+    "202411579": [
+        "ADEPT-Phase1Eval-Ingroup Bias-Group-High",
+        "ADEPT-Phase1Eval-Moral judgement-Group-High"
+    ],
+    "202411539": [
+        "ADEPT-Phase1Eval-Ingroup Bias-Group-High",
+        "ADEPT-Phase1Eval-Moral judgement-Group-High"
+    ],
+    "202411363": [
+        "ADEPT-Phase1Eval-Ingroup Bias-Group-High",
+        "vol-group-target-1-final-eval"
+    ],
+    "202411553": [
+        "ADEPT-Phase1Eval-Ingroup Bias-Group-High",
+        "ADEPT-Phase1Eval-Moral judgement-Group-Low"
+    ],
+    "202411565": [
+        "ADEPT-Phase1Eval-Ingroup Bias-Group-High",
+        "ADEPT-Phase1Eval-Moral judgement-Group-High"
+    ],
+    "202411581": [
+        "ADEPT-Phase1Eval-Ingroup Bias-Group-High",
+        "ADEPT-Phase1Eval-Moral judgement-Group-High",
+        "qol-group-target-2-final-eval",
+        "vol-group-target-1-final-eval"
+    ],
+    "202411359": [
+        "ADEPT-Phase1Eval-Ingroup Bias-Group-High",
+        "ADEPT-Phase1Eval-Moral judgement-Group-High"
+    ],
+    "202411580": [
+        "ADEPT-Phase1Eval-Ingroup Bias-Group-High",
+        "ADEPT-Phase1Eval-Moral judgement-Group-High",
+        "vol-group-target-2-final-eval"
+    ],
+    "202411316": [
+        "ADEPT-Phase1Eval-Moral judgement-Group-Low",
+        "qol-group-target-2-final-eval"
+    ],
+    "202411346": [
+        "ADEPT-Phase1Eval-Moral judgement-Group-Low",
+        "qol-group-target-2-final-eval"
+    ],
+    "202411313": [
+        "ADEPT-Phase1Eval-Moral judgement-Group-Low",
+        "qol-group-target-1-final-eval",
+        "vol-group-target-1-final-eval"
+    ],
+    "202411357": [
+        "ADEPT-Phase1Eval-Moral judgement-Group-Low",
+        "vol-group-target-2-final-eval"
+    ],
+    "202411543": [
+        "ADEPT-Phase1Eval-Moral judgement-Group-Low",
+        "qol-group-target-2-final-eval"
+    ],
+    "202411370": [
+        "ADEPT-Phase1Eval-Moral judgement-Group-Low",
+        "qol-group-target-2-final-eval"
+    ],
+    "202411578": [
+        "ADEPT-Phase1Eval-Moral judgement-Group-High"
+    ],
+    "202411537": [
+        "ADEPT-Phase1Eval-Moral judgement-Group-High",
+        "qol-group-target-2-final-eval"
+    ],
+    "202411534": [
+        "ADEPT-Phase1Eval-Moral judgement-Group-High",
+        "qol-group-target-2-final-eval",
+        "vol-group-target-1-final-eval"
+    ],
+    "202411550": [
+        "ADEPT-Phase1Eval-Moral judgement-Group-High"
+    ],
+    "202411548": [
+        "ADEPT-Phase1Eval-Moral judgement-Group-High"
+    ],
+    "202411546": [
+        "qol-group-target-1-final-eval"
+    ],
+    "202411355": [
+        "qol-group-target-2-final-eval",
+        "vol-group-target-1-final-eval"
+    ],
+    "202411558": [
+        "qol-group-target-2-final-eval"
+    ],
+    "202411562": [
+        "vol-group-target-2-final-eval"
+    ]
 }
 
 def main(mongoDB):
@@ -79,8 +270,8 @@ def main(mongoDB):
         if 'qol' in scenario_id or 'vol' in scenario_id:
             for x in GROUP_TARGETS.get(pid, []):
                 if ('qol' in x and 'qol' in scenario_id) or ('vol' in x and 'vol' in scenario_id):
-                    alignment = requests.get(f'{ST_URL}api/v1/alignment/session?session_id={session_id}&target_id={x}').json().get('score')
-                    group_targets[x] = alignment
+                    alignment = requests.get(f'{ST_URL}api/v1/alignment/session?session_id={session_id}&target_id={x}').json()
+                    group_targets[x] = alignment.get('score')
         else:   
             mj_targets = requests.get(f'{ADEPT_URL}api/v1/get_ordered_alignment?session_id={session_id}&population=false&kdma_id=Moral%20judgement').json()
             io_targets = requests.get(f'{ADEPT_URL}api/v1/get_ordered_alignment?session_id={session_id}&population=false&kdma_id=Ingroup%20Bias').json()
