82a77d5065592a6898960871edfe099d4e7c61d3
kaitlyn.sharo <kaitlyn.sharo@caci.com>
2025-01-02 18:21:15 -0500

    reverted comment

--- ph1_weekly.py
+++ ph1_weekly.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
     db = client.dashboard
 
     # run 'weekly' version of probe matcher
-    # os.system('python3 ph1_probe_matcher.py -i ph1_sim_files -w')
+    os.system('python3 ph1_probe_matcher.py -i ph1_sim_files -w')
 
     # run script that compares humans to adms (RQ1/3/4 column Alignment score (Del|ADM))
     # RQ 5 Alignment score (Participant|ADM (most, least))
