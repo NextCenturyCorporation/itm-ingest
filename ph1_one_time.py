1ee9ccae1420cee1a282b39f83661373a01cb4b9
dereknop <dereknop@gmail.com>
2025-01-08 09:21:07 -0500

    changes so it doesn re run 0_4_8 on prod

--- ph1_one_time.py
+++ ph1_one_time.py
@@ -4,7 +4,10 @@ from pymongo import MongoClient
 from scripts._0_2_8_human_to_adm_comparison import main as compare_probes
 from scripts._0_3_0_percent_matching_probes import main as find_matching_probe_percentage
 from adept_repop_ph1 import main as repop
-
+from scripts._0_4_4_adept_human_adm_compare_DRE_PH1_Server import main as ad_compare
+from scripts._0_4_5_correct_plog_for_incorrect_load import main as correct_plog
+from scripts._0_4_6_rerun_369 import main as rerun_369
+from scripts._0_4_7_ph1_group_targets import main as group_targets
 
 if __name__ == '__main__':
     # set up mongo
@@ -21,8 +24,10 @@ if __name__ == '__main__':
     
     compare_probes(db, 5, False)
 
-    db['itm_version'].update_one({}, {"$set": {'version': '0.4.3'}})
-
+    ad_compare(db)
+    correct_plog(db)
+    rerun_369(db)
+    group_targets(db)
 
     os.system('python3 deployment_script.py')
     compare_probes(db, 4, True)
