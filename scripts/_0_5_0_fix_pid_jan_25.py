c1450b883ade00e13c791e84af01870e19fe30c6
dereknop <dereknop@gmail.com>
2025-01-24 13:06:12 -0500

    script for fixing pid of text results already taken and plog for new participant

---
+++ scripts/_0_5_0_fix_pid_jan_25.py
@@ -0,0 +1,14 @@
+def main(mongo_db):
+    p_log = mongo_db['participantLog']
+    #update plog w new id
+    p_log.update_one(
+        {"ParticipantID": 202411371},
+        {"$set": {"ParticipantID": 202501700}}
+    )
+
+    #update the text scenario results they already took
+    text_scenarios = mongo_db['userScenarioResults']
+    text_scenarios.update_many(
+        {"participantID": "202411371"},  
+        {"$set": {"participantID": "202501700"}}
+    )
