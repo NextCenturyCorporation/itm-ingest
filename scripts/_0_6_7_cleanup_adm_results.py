c7fb40f3beaef671162ebeeb4d9ceafd18ace387
dereknop <dereknop@gmail.com>
2025-04-02 10:59:01 -0400

    multi kdma collect and history update

--- scripts/_0_6_7_cleanup_adm_results.py
+++ scripts/_0_6_7_cleanup_adm_results.py
@@ -9,6 +9,7 @@ def main(mongo_db):
         if adm['adm_name'] == remove_adm:
             # remove the extra adm results from mj2
             adm_collection.delete_one({'_id': adm['_id']})
+            continue
         # remove the uuid portion of the adm names
         elif relevance in adm['adm_name']:
             adm_collection.update_one(
@@ -20,4 +21,36 @@ def main(mongo_db):
                 {'_id': adm['_id']},
                 {'$set': {'adm_name': comparative}}
             )
+        
+        # go through history and clean adm names
+        for i, history_entry in enumerate(adm['history']):
+                if 'parameters' in history_entry and 'adm_name' in history_entry['parameters']:
+                    param_adm_name = history_entry['parameters']['adm_name']
+                    if relevance in param_adm_name:
+                        update_field = f'history.{i}.parameters.adm_name'
+                        adm_collection.update_one(
+                            {'_id': adm['_id']},
+                            {'$set': {update_field: relevance}}
+                        )
+                    elif comparative in param_adm_name:
+                        update_field = f'history.{i}.parameters.adm_name'
+                        adm_collection.update_one(
+                            {'_id': adm['_id']},
+                            {'$set': {update_field: comparative}}
+                        )
+    multi_kdma_collect = mongo_db['multiKdmaData']
+    multi_kdma_collect.delete_many({'admName': remove_adm})
+    multi_kdma_docs = multi_kdma_collect.find({})
+    for doc in multi_kdma_docs:
+        if 'admName' in doc:
+            if relevance in doc['admName']:
+                multi_kdma_collect.update_one(
+                    {'_id': doc['_id']},
+                    {'$set': {'admName': relevance}}
+                )
+            elif comparative in doc['admName']:
+                multi_kdma_collect.update_one(
+                    {'_id': doc['_id']},
+                    {'$set': {'admName': comparative}}
+                )
         
