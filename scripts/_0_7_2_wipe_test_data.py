83fe4ad972a1469b4bd7a7092b246722629d2476
Derek Noppinger <73036954+dereknop@users.noreply.github.com>
2025-04-23 13:53:03 -0400

    Clean test runs from 4/21 (#131)
    * script that can be edited to delete entries from a certain date
    
    * document

---
+++ scripts/_0_7_2_wipe_test_data.py
@@ -0,0 +1,18 @@
+'''
+Any day where we have a lot of testing before a data collection, this script can be tweaked and run to wipe
+away the noisy data. 
+'''
+def main(mongo_db):
+    survey_results = mongo_db['surveyResults']
+
+    start_date = "Mon Apr 21 2025 00:00:00 GMT-0400 (Eastern Daylight Time)"
+    end_date = "Mon Apr 21 2025 23:59:59 GMT-0400 (Eastern Daylight Time)"
+
+    delete_result = survey_results.delete_many({
+        "$or": [
+            {"results.timeComplete": {"$gte": start_date, "$lte": end_date}},
+            {"results.startTime": {"$gte": start_date, "$lte": end_date}}
+        ]
+    })
+
+    print(f"Deleted {delete_result.deleted_count} entries from April 21st, 2025")
