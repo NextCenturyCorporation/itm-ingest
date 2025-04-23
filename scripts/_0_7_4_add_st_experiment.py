09f849003691b0a59c960a879dc181bc289b66bb
Darren Gemoets <darren.gemoets@caci.com>
2025-04-23 17:12:47 -0400

    Bugfixes, output, and clean-up.

--- scripts/_0_7_4_add_st_experiment.py
+++ scripts/_0_7_4_add_st_experiment.py
@@ -15,7 +15,15 @@ Use the new SoarTech endpoint to compare the probe response choices from the res
 Store these three sets of alignment values (once for each seen ADM, four KDMAs per set) in the humanToADMComparison collection.
 
 """
-UPDATE_DATABASE = False
+UPDATE_DATABASE = True
+VERBOSE_OUTPUT = True
+
+@dataclass
+class Pid_data:
+    pid: str
+    text_scenario_id: str
+    text_probes: list
+
 def main(mongo_db):
     text_scenario_collection = mongo_db['userScenarioResults']
     phase1_text_results = text_scenario_collection.find({'evalNumber': {'$in': [5, 6]}})
@@ -37,61 +45,65 @@ def main(mongo_db):
                         if response in mapping:
                             text_probes.append({'probe': {'choice': mapping[response]['choice'], 'probe_id': mapping[response]['probe_id']}})
                         else:
-                            print('Could not find response in mapping!', response, list(mapping.keys()))
+                            print(f"Could not find response in mapping for pid {pid}!", response, list(mapping.keys()))
             comparison_data[pid] = Pid_data(pid, text_scenario_id, text_probes)
-            print(f"Saving comparison data for pid={pid}: {comparison_data[pid]}")
+    print(f"Saved comparison data for {len(comparison_data)} pids.")
 
     survey_results_collection = mongo_db['surveyResults']
     adm_collection = mongo_db['admTargetRuns']
     comparison_collection = mongo_db['humanToADMComparison']
 
     for pid in comparison_data.keys():
+        if VERBOSE_OUTPUT:
+            print()
+        print(f"Processing PID '{pid}'.")
         # Take the above pid and find the documents in surveyResults that it maps to
         survey_results = survey_results_collection.find({'results.pid': pid})
         pid_data: Pid_data = comparison_data[pid]
         # Iterate through the keys in each result until we find the objects with scenarioIndex containing "vol"
         for survey_result in survey_results:
-            for key in survey_result['results']:
-                vol_obj = key.find({'scenarioIndex': {'$regex': 'vol'}})
-                #vol_obj = key.find({'scenarioIndex': {'$in': ['vol-ph1-eval-2', 'vol-ph1-eval-3', 'vol-ph1-eval-4']}})
+            for key, vol_obj in survey_result['results'].items():
+                if not type(vol_obj) == dict:
+                    continue
+                adm_scenario_id = vol_obj.get('scenarioIndex')
+                if not adm_scenario_id or 'vol' not in adm_scenario_id:
+                    continue
                 # There are four such objects, but we're ignoring the "comparison" pageType.
-                if vol_obj.get('pageType') == 'comparison':
+                elif vol_obj.get('pageType') == 'comparison':
                     continue
                 # Use the admName, admTarget, and scenarioIndex (which is the scenario name) to find the matching adm in admTargetRuns
                 adm_name = vol_obj['admName']
                 adm_target = vol_obj['admTarget']
-                adm_scenario = vol_obj['scenarioIndex']
-                pid_data.adm_info.append((vol_obj['admName'], vol_obj['admTarget'], vol_obj['scenarioIndex']))
-                print(f"Appending adm_info for pid={pid}: {(vol_obj['admName'], vol_obj['admTarget'], vol_obj['scenarioIndex'])}")
+                if VERBOSE_OUTPUT:
+                    print(f"-> Appending ADM info for pid={pid}: {adm_name}, {adm_target}, {adm_scenario_id}")
 
                 # In admTargetRuns, collect the ADM responses by searching through history for "Respond to TA1 Probe" commands.
                 adm_runs = adm_collection.find({'evalNumber': {'$in': [5, 6]}, 'adm_name': adm_name, \
-                                                'alignment_target': adm_target, 'scenario': adm_scenario})
+                                                'alignment_target': adm_target, 'scenario': adm_scenario_id})
                 for adm_run in adm_runs:
                     adm_probes = []
                     for history_obj in adm_run['history']:
                         if history_obj['command'] == 'Respond to TA1 Probe':
-                            parms = history_obj['parameters']
-                            # adm_probes = [{'probe': {'choice': 'choice-0', 'probe_id': 'vol-ph1-eval-2-Probe-1'}},
-                            adm_probes.append({'probe': {'choice': parms['choice'], 'probe_id': parms['probe_id']}})
-                            print(f"Appending adm probes: {{'probe': {'choice': parms['choice'], 'probe_id': parms['probe_id']}}}")
+                            probe = {'probe': {'choice': history_obj['parameters']['choice'], 'probe_id': history_obj['parameters']['probe_id']}}
+                            adm_probes.append(probe)
+                            #if VERBOSE_OUTPUT:
+                            #    print(f"Appending adm probe: {probe}")
 
-                            # Use the new SoarTech endpoint to compare the probe response choices from the respective scenario for the ADM (target) to human/text based (aligner).
-                            scores = soartech_utils.get_all_new_alignments(pid_data.text_probes, pid_data.text_scenario_id, adm_probes, adm_scenario)
-                            print(f"Got alignment scores: {scores}")
-                            # Store these three sets of alignment values (once for each seen ADM, four KDMAs per set) in the humanToADMComparison collection
-                            comparison_records = \
-                                comparison_collection.find({'pid': pid, 'text_scenario': pid_data.text_scenario_id, 'adm_scenario': adm_scenario,
-                                                            'adm_alignment_target': adm_target})
-                            print(f"There are {len(comparison_records)} comparison records.")
-                            if UPDATE_DATABASE:
-                                for comparison_record in comparison_records: # should only be 1?
-                                    comparison_collection.update_one({'_id': comparison_record['_id']}, {'$set': {'calibration_scores': scores}})
+                    # Use the new SoarTech endpoint to compare the probe response choices from the respective scenario for the ADM (target) to human/text based (aligner).
+                    scores = soartech_utils.get_all_new_alignments(pid_data.text_probes, pid_data.text_scenario_id, adm_probes, adm_scenario_id)
+                    if VERBOSE_OUTPUT:
+                        print(f"-> Got alignment scores: {scores}")
 
-
-@dataclass
-class Pid_data:
-    pid: str
-    text_scenario_id: str
-    text_probes: list
-    adm_info = []
+                    # Store these three sets of alignment values (once for each seen ADM, four KDMAs per set) in the humanToADMComparison collection
+                    comparison_records = \
+                        comparison_collection.find({'pid': pid, 'text_scenario': pid_data.text_scenario_id, 'adm_scenario': adm_scenario_id,
+                                                    'adm_alignment_target': adm_target})
+                    num_records = 0
+                    for comparison_record in comparison_records:
+                        num_records += 1
+                        if VERBOSE_OUTPUT:
+                            print(f"-> Updating id {comparison_record['_id']} with scores; pid: {pid}; text_scenario: {pid_data.text_scenario_id}; adm_scenario: {adm_scenario_id}; alignment target: {adm_target}")
+                        if UPDATE_DATABASE:
+                            comparison_collection.update_one({'_id': comparison_record['_id']}, {'$set': {'calibration_scores': scores}})
+                    if num_records != 1: # Normally, there should only be 1
+                        print(f"NOTE: There are {num_records} comparison records.")
