40e8bf18022354f3813ce5a484a3b7265743e071
dereknop <dereknop@gmail.com>
2025-08-06 11:44:22 -0400

    repop

---
+++ ph2_repop.py
@@ -0,0 +1,126 @@
+"""
+https://nextcentury.atlassian.net/browse/ITM-1085
+
+This script takes all text based scenario results and adm runs of eval number 8 or greater
+and repopulates the ADEPT server with the probe responses. This script is to be used if the
+ADEPT server is not persisting session ids that are needed to generate comparison scores.
+"""
+
+import requests
+from decouple import config
+from collections import defaultdict
+import utils.db_utils as db_utils
+
+MONGO_URL = config("MONGO_URL")
+ADEPT_URL = config("ADEPT_URL")
+
+
+def main(mongo_db):
+    text_scenarios = mongo_db["userScenarioResults"].find({"evalNumber": {"$gte": 8}})
+    adm_medics = mongo_db["admMedics"].find({"evalNumber": {"$gte": 8}})
+    adm_runs = mongo_db["admTargetRuns"].find({"evalNumber": {"$gte": 8}})
+
+    # group text scenarios by pid
+    participant_groups = defaultdict(list)
+    for result in text_scenarios:
+        participant_groups[result["participantID"]].append(result)
+
+    for participant_id, documents in participant_groups.items():
+        print(
+            f"Processing participant {participant_id} with {len(documents)} documents"
+        )
+
+        if len(documents) != 4:
+            print(
+                f"Warning: Participant {participant_id} has {len(documents)} documents instead of 4"
+            )
+            continue
+
+        # creates new session id and responds to all probes using it
+        sid = (
+            requests.post(f"{ADEPT_URL}api/v1/new_session")
+            .text.replace('"', "")
+            .strip()
+        )
+        for document in documents:
+            probes = []
+            for key, value in document.items():
+                if isinstance(value, dict) and "questions" in value:
+                    probe = value["questions"].get(f"probe {key}", {})
+                    response = probe.get("response", "").replace(".", "")
+                    mapping = probe.get("question_mapping", {})
+                    if response in mapping:
+                        probes.append(
+                            {
+                                "probe": {
+                                    "choice": mapping[response]["choice"],
+                                    "probe_id": mapping[response]["probe_id"],
+                                }
+                            }
+                        )
+            db_utils.send_probes(
+                f"{ADEPT_URL}api/v1/response", probes, sid, document["scenario_id"]
+            )
+            # update the session id on the text based documents
+            mongo_db["userScenarioResults"].update_one(
+                {"_id": document["_id"]}, {"$set": {"combinedSessionId": sid}}
+            )
+
+    for adm_run in adm_runs:
+        sid = (
+            requests.post(f"{ADEPT_URL}api/v1/new_session")
+            .text.replace('"', "")
+            .strip()
+        )
+        # probably not needed but better to update the admMedics collection as well
+        corresponding_medic = mongo_db["admMedics"].find_one(
+            {"admSessionId": adm_run.get("results", {}).get("ta1_session_id")}
+        )
+
+        probes = []
+        for entry in adm_run["history"]:
+            if entry["command"] == "Respond to TA1 Probe":
+                probe = entry["parameters"]
+                probes.append(
+                    {
+                        "probe": {
+                            "choice": probe["choice"],
+                            "probe_id": probe["probe_id"],
+                        }
+                    }
+                )
+
+        db_utils.send_probes(
+            f"{ADEPT_URL}api/v1/response", probes, sid, document["scenario_id"]
+        )
+        if corresponding_medic:
+            mongo_db["admMedics"].update_one(
+                {"_id": corresponding_medic["_id"]}, {"$set": {"admSessionId": sid}}
+            )
+
+        # updates every reference to the old session id in the adm run document
+        mongo_db["admTargetRuns"].update_one(
+            {"_id": adm_run["_id"]},
+            {
+                "$set": {
+                    "results.ta1_session_id": sid,
+                    "history.$[sessionId].response": sid,
+                    "history.$[probeAlign].parameters.session_id": sid,
+                    "history.$[sessionAlign].parameters.session_id": sid,
+                }
+            },
+            array_filters=[
+                {"sessionId.command": "TA1 Session ID"},
+                {"probeAlign.command": "TA1 Probe Response Alignment"},
+                {"sessionAlign.command": "TA1 Session Alignment"},
+            ],
+        )
+
+
+if __name__ == "__main__":
+    from pymongo import MongoClient
+
+    MONGO_URL = config("MONGO_URL")
+    client = MongoClient(MONGO_URL)
+    mongoDB = client["dashboard"]
+    main(mongoDB)
