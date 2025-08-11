84393d30ef7f0ceb3d22295e1817e222734a8afb
dereknop <dereknop@gmail.com>
2025-08-11 15:58:49 -0400

    Revert "eval nums"
    This reverts commit 358f289f581f6bd090837807ce3110defc39614b.

--- scripts/_0_9_0_eval_number_fix.py
+++
@@ -1,6 +0,0 @@
-def main(mongo_db):
-    adm_runs_collection = mongo_db['admTargetRuns']
-    adm_runs_collection.update_many(
-        {'evalNumber': '9'},
-        {"$set": {"evalNumber": 9}}  
-    )
