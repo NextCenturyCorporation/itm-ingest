352da1665c5c6081043beef9bafbcf5fc125ed16
dereknop <dereknop@gmail.com>
2025-08-21 09:21:34 -0400

    pid initial bounds

--- scripts/_0_9_1_current_text_eval_init.py
+++ scripts/_0_9_1_current_text_eval_init.py
@@ -1,8 +1,16 @@
 def main(mongo_db):
     current_text_eval = mongo_db['textEvalVersion']
+    pid_bounds = mongo_db['pidBounds']
 
-    doc = {
+    eval_doc = {
         'eval': 'Phase 2 July 2025 Collaboration'
     }
 
-    current_text_eval.insert_one(doc)
+    current_text_eval.insert_one(eval_doc)
+
+    pid_doc = {
+        'lowPid': 202570100,
+        'highPid': 202570299
+    }
+
+    pid_bounds.insert_one(pid_doc)
