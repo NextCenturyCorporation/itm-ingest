adf636c153f6d55af8f5993594a50e3d3241f447
Daniel <neil.daniel_cn@cacicorenet.com>
2025-08-26 19:22:54 -0500

    fix date repetition in ADM names

--- scripts/_0_8_9_ph2_repop_cleanup.py
+++ scripts/_0_8_9_ph2_repop_cleanup.py
@@ -1,5 +1,4 @@
 from ph2_repop import main as ph2_repop
-from datetime import datetime
 
 def main(mongo_db):
     adm_runs = mongo_db['admTargetRuns']
@@ -8,22 +7,6 @@ def main(mongo_db):
         {'evalNumber': 8, 'adm_name': {'$regex': '__'}},
         [{'$set': {'adm_name': {'$arrayElemAt': [{'$split': ['$adm_name', '__']}, 0]}}}]
     )
-    
-    documents = adm_runs.find({'evalNumber': 8})
-    
-    for doc in documents:
-        current_name = doc.get('adm_name', '')
-        start_time_str = doc.get('evaluation', {}).get('start_time', '')
         
-        if start_time_str and start_time_str.strip() and start_time_str != '-':
-            dt = datetime.strptime(start_time_str.split('.')[0], "%Y-%m-%d %H:%M:%S")
-            new_name = f"{current_name}_{dt.month}_{dt.day}"
-            
-            adm_runs.update_one(
-                {'_id': doc['_id']},
-                {'$set': {'adm_name': new_name}}
-            )
-    
-    
     # repopulate ta1 server
     ph2_repop(mongo_db)
