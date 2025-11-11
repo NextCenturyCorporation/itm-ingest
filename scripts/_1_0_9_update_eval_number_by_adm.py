9cd5f8c346a386f149a127657df45bac0b77f088
dereknop <dereknop@gmail.com>
2025-11-11 09:56:41 -0500

    fix

--- scripts/_1_0_9_update_eval_number_by_adm.py
+++ scripts/_1_0_9_update_eval_number_by_adm.py
@@ -2,7 +2,14 @@ def main(mongo_db):
     adm_runs_collection = mongo_db['admTargetRuns']
     adm_runs_collection.update_many(
         {'adm_name': 'ALIGN-ADM-DirectRegression-Mistral-7B-Instruct-v0.3__d2217c78-78ac-453a-833c-bf3a8ae9f831'},
-        {"$set": {"evalNumber": 14}},
-        {"$set": {"evalName": "October 2025 Aligned ADM re-run"}},
-        {"$set": {"adm_name": "ALIGN-ADM-DirectRegression-Mistral-7B-Instruct-v0.3_10_29"}}
+        {
+            "$set": {
+                "evalNumber": 14,
+                "evalName": "October 2025 Aligned ADM re-run",
+                "adm_name": "ALIGN-ADM-DirectRegression-Mistral-7B-Instruct-v0.3_10_29",
+                "evaluation.adm_name": "ALIGN-ADM-DirectRegression-Mistral-7B-Instruct-v0.3_10_29",
+                "evaluation.evalNumber": "14",
+                "evaluation.evalName": "October 2025 Aligned ADM re-run"
+            }
+        }
     )
