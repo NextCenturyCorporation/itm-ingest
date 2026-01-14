23b0c72873c6fd23502c4ffebb9817e091f41b89
dereknop <dereknop@gmail.com>
2026-01-14 17:20:38 -0500

    1d adm names added

--- scripts/_1_1_7_add_feb_observe_scenario_yamls.py
+++ scripts/_1_1_7_add_feb_observe_scenario_yamls.py
@@ -3,10 +3,16 @@ import os, yaml
 SCENARIOS_FOLDER = 'phase2/feb2026/admrun'
 
 KEEP_BASELINE_ADMS = {
+    #2d
     'ALIGN-ADM-OutlinesBaseline-DeepSeek-R1-Distill-Llama-8B__6d9ffe7b-a882-4846-9d8d-95a1644c76c2',
     'ALIGN-ADM-OutlinesBaseline-Mistral-7B-Instruct-v0.3__89e3b322-6865-41d1-8392-818c727aee6d',
     'ALIGN-ADM-OutlinesBaseline-spectrum-Llama-3.1-8B-v1__9b3bef95-7ff8-4764-852e-1953cc75498e',
-    'ALIGN-ADM-OutlinesBaseline-spectrum-Qwen3-14B-v1__353ae8ef-b2a7-4a54-b890-031858b8c9ce'
+    'ALIGN-ADM-OutlinesBaseline-spectrum-Qwen3-14B-v1__353ae8ef-b2a7-4a54-b890-031858b8c9ce',
+    #1d
+    'ALIGN-ADM-OutlinesBaseline-DeepSeek-R1-Distill-Llama-8B__654f723a-a594-4b32-874a-833540067564',
+    'ALIGN-ADM-OutlinesBaseline-Mistral-7B-Instruct-v0.3__7013f3b9-6c32-4c5a-b140-0ee5a375db54',
+    'ALIGN-ADM-OutlinesBaseline-spectrum-Llama-3.1-8B-v1__babcf1ca-0489-4426-be9e-bf2947f3a2f9',
+    'ALIGN-ADM-OutlinesBaseline-spectrum-Qwen3-14B-v1__d37bf24d-273b-4133-b469-b6a5caef3635'
 }
 
 KEEP_ALIGNED_ADMS = {
@@ -14,7 +20,13 @@ KEEP_ALIGNED_ADMS = {
     'ALIGN-ADM-Ph2-ComparativeRegression-BertRelevance-Mistral-7B-Instruct-v0.3__bf7d613c-f38c-42de-8eef-cec5868cccb1',
     'ALIGN-ADM-Ph2-ComparativeRegression-BertRelevance-spectrum-Llama-3.1-8B-v1__f3e4cce5-933f-4a49-a27b-c15a08b76492',
     'ALIGN-ADM-Ph2-ComparativeRegression-BertRelevance-spectrum-Qwen3-14B-v1__783573b5-96c9-485b-a4f6-eb81ddc550d5',
-    'ALIGN-ADM-Ph2-DirectRegression-BertRelevance-Mistral-7B-Instruct-v0.3__596c459d-b735-4a7a-8bf6-815d804e4092'
+    'ALIGN-ADM-Ph2-DirectRegression-BertRelevance-Mistral-7B-Instruct-v0.3__596c459d-b735-4a7a-8bf6-815d804e4092',
+    #1d
+    'ALIGN-ADM-Ph2-ComparativeRegression-BertRelevance-DeepSeek-R1-Distill-Llama-8B__30b1f803-cd61-4eaa-a9de-ed18cd944067',
+    'ALIGN-ADM-Ph2-ComparativeRegression-BertRelevance-Mistral-7B-Instruct-v0.3__84d41345-5964-4f15-92bc-2b839911c546',
+    'ALIGN-ADM-Ph2-ComparativeRegression-BertRelevance-spectrum-Llama-3.1-8B-v1__b1403cb2-d254-40c9-9793-315adf98d1f3',
+    'ALIGN-ADM-Ph2-ComparativeRegression-BertRelevance-spectrum-Qwen3-14B-v1__3d831590-27ae-4be8-a81f-722e193cdce3',
+    'ALIGN-ADM-Ph2-DirectRegression-BertRelevance-Mistral-7B-Instruct-v0.3__89d49877-0eb0-4919-aac1-92860615d131'
 }
 
 def main(mongo_db):
