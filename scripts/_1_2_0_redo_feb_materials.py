30e6c50db3acb620a8cd13bf91691de6396ef2cf
dereknop <dereknop@gmail.com>
2026-01-29 09:52:13 -0500

    512 duplicates removed

--- scripts/_1_2_0_redo_feb_materials.py
+++ scripts/_1_2_0_redo_feb_materials.py
@@ -4,6 +4,5 @@ def main(mongo_db):
     # delete the old survey version 10
     mongo_db["delegationConfig"].delete_one({"_id": "delegation_v10.0"})
     print("Deleted old survey version 10 config file.")
-
-    
+    mongo_db['admMedics'].delete_many({'evalNumber': 15})
     make_new_survey(mongo_db)
