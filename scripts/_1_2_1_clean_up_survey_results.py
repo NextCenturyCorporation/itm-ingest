7d797eb122099a37ffa55b8334289afb5a434747
Tabernero <kayla.tabernero_cn@cacicorenet.com>
2026-02-27 17:31:01 -0500

    WIP: enhanced code readability, accuracy, and organization

--- scripts/_1_2_1_clean_up_survey_results.py
+++ scripts/_1_2_1_clean_up_survey_results.py
@@ -1,60 +1,56 @@
+import pymongo
 
 '''
 This script cleans up all the entries in surveyResults and scenarioResults collections 
-that do not have a participantID in participantLogs OR its participantID field value is null
+that do not have a participantID in participantLogs OR its participantID field value is null or does not exist
 
-** CLEAN UP UNECESSARY CODE & MAKE IT ORGANIZED 
+** FACT CHECK FIELD NAMES && TYPES W/ MONGODB
 
 '''
+
+def get_valid_participant_ids(pid_collection):
+    # get valid pids from participantLog
+    log_pids = pid_collection.find({"ParticipantID" : {"$exists": True, "$nin": None}})
+    valid_pids = set()
+
+    for doc in log_pids:
+        valid_pids.add(doc["ParticipantID"])
+
+    return valid_pids
+
+def delete_null_pids(collection, pid_field):
+    # delete all entries where pid has a null value or does not exist
+    survey_collection.delete_many({
+        "$or": [
+            {pid_field: None},
+            {pid_field: {"$exists": False}}
+        ]
+    })
+
+def delete_invalid_pids(collection, pid_field, valid_pids):
+    # delete entires where pid is NOT in valid_pids from Participant Logs
+    # AND if evalNumber <= 4 (we want data that's pre-DRE only)
+    collection.delete_many({
+        pid_field: {"$nin": valid_pids},
+        "evalNumber": {"$lte": 4}
+        })
+
+
 def main(mongo_db):
     # get all necessary collections and variables
     survey_collection = mongo_db['surveyResults']
     scenarioResult_collection = mongo_db['userScenarioResults']
     pid_collection = mongo_db['participantLog']
     
-    # get valid pids from participantLog
-    log_pids = pid_collection.find({"ParticipantID" : {"$exists": true, "$nin": null}})
-    valid_pids = set()
-    for doc in log_pids:
-        valid_pids.add(doc["ParticipantID"])
+    # get a set of valid PID from participantLog collection
+    valid_pids = get_valid_participant_ids(pid_collection)
 
-    # delete all entries from both surveyResults and scenarioResults that's pid is null
-    survey_null = survey_collection.find({"results.pid": {"$exists": true, "$eq": null}})
-    ids_to_delete = []
-    for doc in survey_null:
-        ids_to_delete.append(doc["_id"])
+    # remove null/missing PIDs
+    delete_null_pids(survey_collection, "results.pid")
+    delete_null_pids(scenario_collection, "participantID")
 
-    survey_collection.delete_many("_id": {"$in": ids_to_delete})
-
-    scenario_null = scenarioResult_collection.find("participantID": {"$exists": true, "$eq": null})
-    delete_null_ids = []
-    for doc in scenario_null:
-        delete_null_ids.append(doc["_id"])
-    scenarioResult_collection.delete_many("_id": {"$in": delete_null_ids})
-
-    # get surveyResults and scenarioResults non-null ids & cross reference them in valid_pids
-    survey_pid = survey_collection.find({"results.pid": {"$exists": true, "$nin": null}})
-    delete_survey_ids = []
-    for doc in survey_pid:
-        pid = doc["results"].get("pid")
-        if pid not in valid_pids:
-            # add pid to ids to delete IFF data is pre-DRE (eval number 4)
-            if doc["evalNumber"] != null and doc["evalNumber"] < 4:
-                delete_survey_ids.append(doc["_id"])
-
-    # delete non-valid survey ids
-    survey_collection.delete_many("_id": {"$in": delete_survey_ids})
-
-    scenario_pid = survey_collection.find({"results.pid": {"$exists": true, "$nin": null}})
-    delete_scenario_ids = []
-    for doc in scenario_pid:
-        pid = doc["participantID"]
-        if pid not in valid_pids:
-            # add pid to ids to delete IFF data is pre-DRE (eval number 4)
-            if doc["evalNumber"] != null and doc["evalNumber"] < 4:
-                delete_scenario_ids.append(doc["_id"])
-
-     # delete non-valid scenario ids
-    scenarioResult_collection.delete_many("_id": {"$in": delete_scenario_ids})
+    # remove invalid PIDs (ONLY pre-DRE data)
+    delete_invalid_pids(survey_collection, "results.pid", valid_pids)
+    delete_invalid_pids(scenario_collection, "participantID", valid_pids)
     
 
