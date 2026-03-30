3b28576ab4b0f8094fd9e39a9a2eaa0b297bdb0d
dereknop <dereknop@gmail.com>
2026-03-30 17:25:09 -0400

    version 11

--- scripts/_1_3_1_add_del_april.py
+++ scripts/_1_3_1_add_del_april.py
@@ -1,2 +1,7 @@
+from delegation_survey.april_handmade_adms import main as april_medics
+from delegation_survey.update_survey_config import version11_setup
+from scripts._1_2_2_post_scenario_measures import main as post_scenario
 def main(mongo_db):
-    pass
+    april_medics(mongo_db)
+    version11_setup(auto_confirm=True)
+    post_scenario(mongo_db)
