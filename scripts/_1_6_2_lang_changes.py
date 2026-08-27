270650d9abd68ff87e244a34f8360ad5d591d2fd
dereknop <dereknop@gmail.com>
2026-08-27 16:05:28 -0400

    added a bunch to the lang dict

---
+++ scripts/_1_6_2_lang_changes.py
@@ -0,0 +1,16 @@
+lang_dict = {
+    'Patient A has a chest wound and are gasping for breath.': 'Patient A has a chest wound and is gasping for breath.',
+    'Patient B has a chest wound and are gasping for breath.': 'Patient B has a chest wound and is gasping for breath.',
+    'Patient A has no visible bleeding but are not breathing after their airway has been repositioned.': 'Patient A has no visible bleeding but is not breathing after their airway has been repositioned.',
+    'Patient B has no visible bleeding but are not breathing after their airway has been repositioned.': 'Patient B has no visible bleeding but is not breathing after their airway has been repositioned.',
+    'Patient A has no visible bleeding but are not breathing': 'Patient A has no visible bleeding but is not breathing',
+    'Patient B has no visible bleeding but are not breathing': 'Patient B has no visible bleeding but is not breathing',
+    'Patient A has a chest wound and are gasping for breath.': 'Patient A has a chest wound and is gasping for breath.',
+    'Patient B has a chest wound and are gasping for breath.': 'Patient B has a chest wound and is gasping for breath.',
+    'JLTVs': 'tactical vehicles',
+    'a SMAW fired into': 'fire in',
+    'They are a POW': 'They are a PW',
+    'They are a US civilian contractor who works for the US military': 'They are a Canadian civilian contractor who works for the Canadian military'
+}
+def main(mongo_db):
+    pass
