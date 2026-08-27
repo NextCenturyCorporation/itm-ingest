01737472da0f421ec97feddb56a3d8eff3683754
dereknop <dereknop@gmail.com>
2026-08-27 16:19:57 -0400

    script

--- scripts/_1_6_2_lang_changes.py
+++ scripts/_1_6_2_lang_changes.py
@@ -13,4 +13,25 @@ lang_dict = {
     'They are a US civilian contractor who works for the US military': 'They are a Canadian civilian contractor who works for the Canadian military'
 }
 def main(mongo_db):
-    pass
+    survey = mongo_db['delegationConfig'].find_one({'_id': 'delegation_v13.0'})
+    for page in survey['survey']['pages']:
+            for row in page['elements'][0]['rows']:
+                for old_text, new_text in lang_dict.items():
+                    row['probe_unstructured'] = row['probe_unstructured'].replace(old_text, new_text)
+
+    mongo_db['delegationConfig'].update_one(
+        {'_id': 'delegation_v13.0'},
+        {'$set': {'survey.pages': survey['survey']['pages']}}
+    )
+
+    text_configs = list(mongo_db['textBasedConfig'].find({'eval': 'Phase 2 Canada Evaluation'}))
+
+    for config in text_configs:
+         for page in config['pages']:
+              for old_text, new_text in lang_dict.items():
+                   page['elements'][0]['probe_unstructured'] = page['elements'][0]['probe_unstructured'].replace(old_text, new_text)
+
+         mongo_db['textBasedConfig'].update_one(
+             {'_id': config['_id']},
+             {'$set': {'pages': config['pages']}}
+         )
